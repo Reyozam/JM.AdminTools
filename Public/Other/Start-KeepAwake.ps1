78e55a2439dc092ee81fe5346a41334e893005d0
Reyozam <km5078@engie.com>
2023-03-06 09:43:13 +0100

    2023.03.06

--- Public/Other/Start-KeepAwake.ps1
+++ Public/Other/Start-KeepAwake.ps1
@@ -1,38 +1,41 @@
-﻿function Start-KeepAwake {
-    [CmdletBinding()]
-    param (
-        [int]$Minutes
-    )
+﻿function Start-KeepAwake
+{
+  [CmdletBinding()]
+  param (
+    [int]$Minutes
+  )
 
-    if ($Minutes -ne 0)
+  if ($Minutes -ne 0)
+  {
+    Write-Verbose "Keep Awake for $Minutes minutes ..." -Verbose
+    $timeSpan = New-TimeSpan -Minutes $Minutes
+    $stopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+    $stopWatch.Start()
+
+    $wsh = New-Object -ComObject WScript.Shell
+    do
     {
-        Write-Verbose "Keep Awake for $Minutes minutes ..." -Verbose
-        $timeSpan = New-TimeSpan -Minutes $Minutes
-        $stopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+      $Remain = $TimeSpan - $stopWatch.Elapsed
+      Write-Verbose ('{0}m:{1}s remaining...' -f $Remain.Minutes, $Remain.Seconds) -Verbose
 
-        $wsh = New-Object -ComObject WScript.Shell
-        while ($stopWatch.Elapsed -le $timeSpan)
-        {
-          $wsh.SendKeys('+{F15}')
-          Start-Sleep -seconds 59
+      $wsh.SendKeys('+{F15}')
 
-          $Remain = $TimeSpan - $stopWatch.Elapsed
+      Start-Sleep -Seconds 60
 
-          Write-Verbose ("{0}m:{1}s remaining..." -f $Remain.Minutes,$Remain.Seconds) -Verbose
-
-        }
-    }
-    else
+    } while ( $stopWatch.Elapsed -lt $timeSpan)
+  }
+  else
+  {
+    Write-Verbose 'Keep Awake until manually stop !' -Verbose
+    $wsh = New-Object -ComObject WScript.Shell
+    while ($true)
     {
-        Write-Verbose "Keep Awake until manually stop !" -Verbose
-        $wsh = New-Object -ComObject WScript.Shell
-        while ($true)
-        {
-          $wsh.SendKeys('+{F15}')
-          Start-Sleep -seconds 59
+      $wsh.SendKeys('+{F15}')
+      Start-Sleep -Seconds 59
 
-          $Remain = $TimeSpan - $stopWatch.Elapsed
+      $Remain = $TimeSpan - $stopWatch.Elapsed
 
-        }
     }
+  }
 }
+
